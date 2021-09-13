460f153c65fd530bc8d1bb2b77f59d1d9fedb982
Belén Pinilla <m.pinilla04@ufromail.cl>
2021-09-13 16:32:50 -0300

    ahora si

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,5 +1,8 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("muy buenas tardes, con todo respeto");
+        System.out.println("Rulos estuvo aquí");
+        System.out.println(" Belén Pinilla ");
     }
 }
