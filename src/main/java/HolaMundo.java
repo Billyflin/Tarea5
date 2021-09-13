7805e2b1e2979332713ecd21ea4f1b84c0d42a24
TomasB22 <t.baeza01@ufromail.cl>
2021-09-13 15:40:15 -0300

    Se agrega nombre y apellido.

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,5 +1,9 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        String nombre = "Tomás";
+        String apellido = "Baeza";
+        System.out.println ("Mi nombre es "+nombre+apellido);
     }
+
 }
