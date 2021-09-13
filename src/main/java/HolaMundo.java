0f83756270dfaef3d25c190dce578868df8056bb
Billy <79352801+Billyflin@users.noreply.github.com>
2021-09-13 16:27:35 -0300

    Delete src directory

--- src/main/java/HolaMundo.java
+++
@@ -1,9 +0,0 @@
-public class HolaMundo {
-    public static void main(String[] args) {
-        System.out.println("Hola mundo");
-        System.out.println(nombre());
-    }
-    public static String nombre(){
-     return "Billy Martínez";
-    }
-}
