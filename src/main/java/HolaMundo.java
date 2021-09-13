00550387612618e74259d612ebc0abac42784954
Billy <79352801+Billyflin@users.noreply.github.com>
2021-09-13 17:24:52 -0300

    Agregado nombre y apellido

--- src/main/java/HolaMundo.java
+++ src/main/java/HolaMundo.java
@@ -1,10 +1,11 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        System.out.println(name());
+        System.out.println(name()+apellido());
     }
 
+    private static String apellido() {return " Torres";}
     private static String name() {
-        return "Billy Martínez";
+        return "Danko";
     }
 }
